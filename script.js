b27bd2294e4c1ae77c468f8e632a1f8c1a55f4ee
Hazhir Naseri <94365660+hazhir00@users.noreply.github.com>
2024-12-12 17:58:11 +0330

    Add files

---
+++ script.js
@@ -0,0 +1,20 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+};
+
+function playPause() {
+    if (ctrlIcon.classList.contains("fa-pause")) {
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    } else {
+        song.paly();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+}
