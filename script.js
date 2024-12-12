03e16af15bba8bd11e9a5116444feaac7b895036
Hazhir Naseri <94365660+hazhir00@users.noreply.github.com>
2024-12-12 20:41:35 +0330

    finished project

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ song.onloadedmetadata = function () {
 };
 
 function playPause() {
-    if (ctrlIcon.classList.contains("fa-pause")) {
-        song.pause();
-        ctrlIcon.classList.remove("fa-pause");
-        ctrlIcon.classList.add("fa-play");
-    } else {
-        song.paly();
-        ctrlIcon.classList.add("fa-pause");
+    if (song.paused) {
+        song.play();
         ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+    } else {
+        song.pause();
+        ctrlIcon.classList.add("fa-play");
+        ctrlIcon.classList.remove("fa-pause");
     }
 }
+
+if (song.play) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+}
+
+progress.onchange = function () {
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+};
